3a9ceed6fc87319426d23c6fa4fb560eab1c6eb9
Chad Jablonski <chad@dinocore.net>
2019-06-24 08:15:44 -0700

    Fetch additional fields on auth

--- lib/pinterest/omniauth.rb
+++ lib/pinterest/omniauth.rb
@@ -3,25 +3,37 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Pinterest < OmniAuth::Strategies::OAuth2
-      option :client_options, {
-        :site => 'https://api.pinterest.com/',
-        :authorize_url => 'https://api.pinterest.com/oauth/',
-        :token_url => 'https://api.pinterest.com/v1/oauth/token'
-      }
-
-      def request_phase
-        options[:scope] ||= 'read_public,write_public'
-        options[:response_type] ||= 'token'
-        #options[:state] ||= '22'
-        super
-      end
+      option :client_options,
+             site: 'https://api.pinterest.com/',
+             authorize_url: 'https://api.pinterest.com/oauth/',
+             token_url: 'https://api.pinterest.com/v1/oauth/token'
 
       uid { raw_info['id'] }
 
       info { raw_info }
 
+      def request_phase
+        options[:scope] ||= 'read_public'
+        options[:response_type] ||= 'code'
+        super
+      end
+
+      def authorize_params
+        super.tap do |params|
+          %w[redirect_uri].each do |v|
+            params[:redirect_uri] = request.params[v] if request.params[v]
+          end
+        end
+      end
+
+      def ssl?
+        true
+      end
+
       def raw_info
-        @raw_info ||= access_token.get('/v1/me/').parsed['data']
+        fields = 'first_name,id,last_name,url,account_type,username,bio,image'
+        @raw_info ||=
+          access_token.get("/v1/me/?fields=#{fields}").parsed['data']
       end
     end
   end
